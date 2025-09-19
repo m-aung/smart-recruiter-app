7ac671bc9871f36d7b6f1b9e770b76c720b043ad
m-aung <myothuwinaung1991@gmail.com>
2025-09-18 23:58:51 -0400

    remove od packages

--- packages/package-b/index.test.js
+++
@@ -1,8 +0,0 @@
-const { greetUpper } = require('./index');
-
-if (greetUpper('World') !== 'HELLO, WORLD!') {
-  console.error('package-b test failed');
-  process.exit(1);
-}
-
-console.log('package-b test passed');
